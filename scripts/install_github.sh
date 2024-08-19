cbe6f346d32bb969c037ccebcf1a35752a53e05c
Felipe Pimentel <fpimentel88@gmail.com>
2024-08-19 01:14:14 -0300

    chore: Update GitHub CLI installation script
    This commit adds a new script `install_github.sh` to install the GitHub CLI. The script includes the following steps:
    - Adding the GitHub CLI repository
    - Updating and installing the GitHub CLI
    
    ```

---
+++ scripts/install_github.sh
@@ -0,0 +1,7 @@
+# Adicionar repositório do GitHub CLI
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:git-core/ppa
+
+# Atualizar e instalar o GitHub CLI
+sudo apt-get update
+sudo apt-get install -y gh
